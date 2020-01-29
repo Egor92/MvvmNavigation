58a6d5ffabfdbf5965301eacd0b4b76ed2cbb9c1
Novikov Egor <novikov.ea@mail.ru>
2020-01-29 23:00:59 +0300

    CodeFactor badge added #28

--- scripts/PushTag_AssemblyVersion_ToMaster.ps1
+++ scripts/PushTag_AssemblyVersion_ToMaster.ps1
@@ -1,7 +1,7 @@
 $tagName = "release-$env:assembly_version"
-Write-Host "Push tag "$tagName
+Write-Verbose "Push tag "$tagName
 git config --global credential.helper store
 Add-Content "$HOME\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
 git tag $tagName "$env:APPVEYOR_REPO_COMMIT"
 git push origin $tagName
-Write-Host "Tag "$tagName" was pushed"
+Write-Verbose "Tag "$tagName" was pushed"
