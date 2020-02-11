d281de2677f2ec1ac32ce388bac2658e5c8c5b16
Novikov Egor <novikov.ea@mail.ru>
2020-02-11 23:49:44 +0300

    Target framework set to net461 and netcoreapp3.0 #38

--- code/test/UINavigation.Tests.Common/ExceptionMessages.cs
+++ code/test/UINavigation.Tests.Common/ExceptionMessages.cs
@@ -1,10 +1,23 @@
-﻿namespace Egor92.UINavigation.Tests.Common
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Egor92.UINavigation.Tests.Common
 {
     public static class ExceptionMessages
     {
         public static string NullArgument(string argumentName)
         {
-            return $"Value cannot be null.\r\nParameter name: {argumentName}";
+            if (RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework"))
+            {
+                return $"Value cannot be null.\r\nParameter name: {argumentName}";
+            }
+
+            if (RuntimeInformation.FrameworkDescription.StartsWith(".NET Core"))
+            {
+                return $"Value cannot be null. (Parameter '{argumentName}')";
+            }
+
+            throw new ArgumentException("Unhandled framework version", nameof(argumentName));
         }
     }
 }
