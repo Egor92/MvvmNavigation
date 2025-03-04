c660dec27aa508b63faa94ffc9e04aeff4c6923f
Novikov Egor <novikov.ea@mail.ru>
2025-03-04 11:09:31 +0300

    Navigate (extension method) returns view and ViewModel #119

--- code/src/MvvmNavigation.Abstractions/NavigationData.cs
+++ code/src/MvvmNavigation.Abstractions/NavigationData.cs
@@ -2,6 +2,12 @@
 {
     public record NavigationData
     {
+        public void Deconstruct(out object view, out object viewModel)
+        {
+            view = View;
+            viewModel = ViewModel;
+        }
+
         public required object View { get; init; }
 
         public required object ViewModel { get; init; }
